9306a19ce3836ad5578c07ffa671206115face22
sk-ys <yskrmt@gmail.com>
2026-02-22 22:12:42 +0900

    Fix private scope in issues controller patch

--- lib/extra_notes/issues_controller_patch.rb
+++ lib/extra_notes/issues_controller_patch.rb
@@ -2,24 +2,25 @@ module ExtraNotes
   module IssuesControllerPatch
     def self.included(base)
       base.after_action :save_extra_note, only: [:update]
+      base.send(:private, :save_extra_note)
+    end
 
-      private
+    private
 
-      def save_extra_note
-        return unless params.key?(:extra_note_type)
-        return unless User.current.allowed_to?(:add_extra_notes, @issue.project) || User.current.allowed_to?(:edit_extra_notes, @issue.project)
+    def save_extra_note
+      return unless params.key?(:extra_note_type)
+      return unless User.current.allowed_to?(:add_extra_notes, @issue.project) || User.current.allowed_to?(:edit_extra_notes, @issue.project)
 
-        last_journal = @issue.journals.last
-        return unless last_journal
+      last_journal = @issue.journals.last
+      return unless last_journal
 
-        note_type_id = params[:extra_note_type].presence
-        if note_type_id && ExtraNotesHelper.enabled_note_types.any? { |t| t['id'] == note_type_id }
-          attr = ExtraJournalAttribute.find_or_initialize_by(journal_id: last_journal.id)
-          attr.note_type = note_type_id
-          attr.save!
-        else
-          last_journal.extra_attribute&.destroy
-        end
+      note_type_id = params[:extra_note_type].presence
+      if note_type_id && ExtraNotesHelper.enabled_note_types.any? { |t| t['id'] == note_type_id }
+        attr = ExtraJournalAttribute.find_or_initialize_by(journal_id: last_journal.id)
+        attr.note_type = note_type_id
+        attr.save!
+      else
+        last_journal.extra_attribute&.destroy
       end
     end
   end
